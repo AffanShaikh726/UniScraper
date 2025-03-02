769fa27ed6e8a4e99067feb381b145b7e1f5a3fe
AffanShaikh726 <affanshaikh0726@gmail.com>
2025-03-03 00:49:34 +0530

    added a caching mechanism

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, jsonify, render_template
+from flask_caching import Cache
 from scraper import scrape_data
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-# data = scrape_data()
+@cache.cached(timeout=2*3600)
+def get_cached_data():
+    print("cached")
+    return scrape_data()
 
-# Method 1: Read entire file as string
-with open("jsonFormat.txt", "r", encoding="utf-8") as file:
-    data = file.read()
+# # Method 1: Read entire file as string
+# with open("jsonFormat.txt", "r", encoding="utf-8") as file:
+#     data = file.read()
 
 @app.route('/')
 def index():
-    return render_template('index.html',content="")
+    return render_template('index.html', content="")
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
     print("API endpoint hit")
+    data = get_cached_data()
     return data
 
 if __name__ == '__main__':
