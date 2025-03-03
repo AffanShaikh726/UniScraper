de88a61fe50881f06e8018ec9c35594e71696c71
AffanShaikh726 <affanshaikh0726@gmail.com>
2025-03-03 14:42:13 +0530

    deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from openAI import jsonifyDataAI
-
-text = ""
-# Method 1: Read entire file as string
-with open("baseData.txt", "r", encoding="utf-8") as file:
-    text = file.read()
-
-print(jsonifyDataAI(text))
